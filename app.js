7df5b40e8f7ef58a2d803e6094faced9771f2637
Andy <willisustc@gmail.com>
2020-09-10 13:20:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ var app = express();
 app.get('/', function(req, res){
   res.send('Hello World');
 });
- 
+app.get('/health', function(req, res){
+  res.send('Healthy');
+});
 app.listen(8081,'0.0.0.0');
